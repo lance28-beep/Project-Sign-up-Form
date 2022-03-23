1972c355ea484bb96676383f46885852c8ae4f6e
Lance Valle <vallelance8@gmail.com>
2022-03-24 06:37:36 +0800

    add form validation using js

--- dist/script.js
+++ dist/script.js
@@ -1,8 +1,23 @@
 const form = document.querySelector('form')
 const input = document.querySelectorAll('input')
 const button = document.querySelector('button')
+const error = document.querySelectorAll('.error')
 
 button.addEventListener('click', () => {
-  // removed old messages
+  const valid = form.querySelectorAll('input:valid')
+  const invalid = form.querySelectorAll('input:invalid')
+
+  //validate all inputs
   form.classList.add('validate')
+
+  //if valid
+  valid.forEach((input) => {
+    let nextSibling = input.nextElementSibling
+    nextSibling.classList.add('undisplay-error')
+  })
+
+  invalid.forEach((input) => {
+    let nextSibling = input.nextElementSibling
+    nextSibling.classList.remove('undisplay-error')
+  })
 })
