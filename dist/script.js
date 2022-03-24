2248232618eb62f794f70ab1d2dc1430fd9198ce
Lance Valle <vallelance8@gmail.com>
2022-03-24 18:13:25 +0800

    final revised design

--- dist/script.js
+++ dist/script.js
@@ -2,6 +2,8 @@ const form = document.querySelector('form')
 const input = document.querySelectorAll('input')
 const button = document.querySelector('button')
 const error = document.querySelectorAll('.error')
+const passwordOne = document.getElementById('password')
+const passwordTwo = document.getElementById('cpassword')
 
 button.addEventListener('click', () => {
   const valid = form.querySelectorAll('input:valid')
@@ -13,11 +15,20 @@ button.addEventListener('click', () => {
   //if valid
   valid.forEach((input) => {
     let nextSibling = input.nextElementSibling
+    input.classList.remove('border-error')
     nextSibling.classList.add('undisplay-error')
   })
 
   invalid.forEach((input) => {
     let nextSibling = input.nextElementSibling
+    input.classList.add('border-error')
     nextSibling.classList.remove('undisplay-error')
   })
+
+  // Match Passwords
+  if (passwordOne.value !== passwordTwo.value) {
+    passwordOne.classList.add('border-error')
+    passwordTwo.classList.add('border-error')
+    error[0].classList.remove('undisplay-error')
+  }
 })
